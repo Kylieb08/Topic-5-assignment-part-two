770964b4d3ffb5fd2d4b587632fb78a34e0a51cb
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-21 14:08:49 -0500

    done the assignment

--- Topic 5 assignment part two/Program.cs
+++ Topic 5 assignment part two/Program.cs
@@ -6,8 +6,17 @@
         {
             //Variables
             int secretNumber = 7;
+            int guess;
 
             Console.WriteLine("Hello. In order to continue, you must guess a number between 1 and 10");
+            Console.WriteLine("Please enter your guess here: ");
+            Int32.TryParse(Console.ReadLine(), out guess);
+
+            if (guess == secretNumber)
+            {
+                Console.WriteLine("Correct");
+            }
+            else { Console.WriteLine("Incorrect. the number was " + secretNumber); }
         }
     }
 }
