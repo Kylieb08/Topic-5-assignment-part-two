c32518bcaf94140b414f756035b28adc8c043643
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-21 13:59:30 -0500

    Add project files.

---
+++ Topic 5 assignment part two/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Topic_5_assignment_part_two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            int secretNumber = 7;
+
+            Console.WriteLine("Hello. In order to continue, you must guess a number between 1 and 10");
+        }
+    }
+}
